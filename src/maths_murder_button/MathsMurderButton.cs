80fbfdfb6c1c3e2fd9d5f925c48e240c9267fcf2
Christos Nicolaou <chris.anicolaou@yahoo.co.uk>
2023-09-30 22:40:23 +0100

    Creates theme WIP

---
+++ src/maths_murder_button/MathsMurderButton.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class MathsMurderButton : Button
+{
+    [Export] private Texture2D _defaultCursor;
+    [Export] private Texture2D _hoverCursor;
+
+    public override void _Ready()
+    {
+        
+    }
+}
