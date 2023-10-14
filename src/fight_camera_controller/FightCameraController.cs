ed74fb96dd59752c8578a01d37fba8cc401b58d7
Christos Nicolaou <chris.anicolaou@yahoo.co.uk>
2023-10-14 23:47:04 +0100

    Remove zoom from FightCamera as it causes more problems than it solves. Adjust offset of camera fight bounds

--- src/fight_camera_controller/FightCameraController.cs
+++ src/fight_camera_controller/FightCameraController.cs
@@ -32,26 +32,5 @@ public partial class FightCameraController : Node2D
         
         // While we're here, lets lerp to the midpoint of both players
         CinematicCamera.Offset = CinematicCamera.Offset.Lerp(new Vector2(Mathf.Clamp(xMidpoint, _fightBounds.X, _fightBounds.Y), CinematicCamera.Offset.Y), (float)delta * _lerpSpeed);
-        
-
-        // 2 - If distance > viewport size, find multiplier required to get viewport size > distance
-        if (distanceBetweenFighters.X + _boundaryFeather <= _viewportSize.X) return;
-        
-        var multiplier = 1f;
-        // For some reason, as the player approaches the edge, the camera can't keep up. Adding a linear factor that
-        // increases as the distance increases helps mitigate this
-        var multiplierFactor = 1f;
-        while (distanceBetweenFighters.X + _boundaryFeather > _viewportSize.X / multiplier)
-        {
-            multiplier -= 0.01f;
-            multiplierFactor -= 0.01f;
-        }
-
-        multiplier = Mathf.Clamp(multiplier * multiplierFactor, 0.01f, 1);
-        // 3 - set destination to that multiplier
-        _destination = new Vector2(multiplier, multiplier);
-        
-        // 4 - Lerp to destination
-        CinematicCamera.Zoom = CinematicCamera.Zoom.Lerp(_destination, (float)delta * _lerpSpeed);
     }
 }
